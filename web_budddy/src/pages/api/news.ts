e3f9e07bcf561b83f1819262f0f4dbb07d5614c0
pandarudra <rudrapanda8206@gmail.com>
2025-02-28 15:32:13 +0530

    news

--- web_budddy/src/pages/api/news.ts
+++ web_budddy/src/pages/api/news.ts
@@ -1,19 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
+// import axios from "axios";
+import { scrapeNews } from "./utils/scrap";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines`, {
-      params: {
-        country: "us",
-        apiKey: process.env.NEWS_API_KEY,
-      },
-    });
+    // const news = await scrapeNews("cricket", ".dataList");
 
-    res.status(200).json(response.data.articles);
+    const news = await scrapeNews("entertainment", ".listingPage");
+    res.status(200).json(news);
   } catch {
     res.status(500).json({ error: "Failed to fetch news" });
   }
